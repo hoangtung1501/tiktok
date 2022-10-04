17da4f42649d49a60994235754100d57ee5e3071
tung <tung@gmail.com>
2022-10-04 23:29:01 +0700

    Bài 63: Dựng khung DefaultLayout dự án Tiktok

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,13 @@
-function Sidebar() {
-    return <h2>Sidebar</h2>;
-}
+import styles from './Sidebar.module.scss';
+import classNames from 'classnames/bind';
 
+const cx = classNames.bind(styles);
+
+function Sidebar() {
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>Sidebar</h2>
+        </aside>
+    );
+}
 export default Sidebar;
